c969780f056a9fc0fa04293187eb8ba1e05e9a45
chengziyue <huayueczy@163.com>
2019-06-10 16:38:21 +0800

    微分销系统

--- wxf-vip/src/main/java/com/qfedu/vip/service/IOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.qfedu.vip.service;
-
-import com.qfedu.vip.po.OrderInfo;
-
-public interface IOrderService {
-    void addOrder(OrderInfo orderInfo)throws Exception;
-
-    void updateOrderState(String orderId)throws Exception;
-}
